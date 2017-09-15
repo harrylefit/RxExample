bfd0286dccee985169b4872b45a8a980a00426a6
Harry Le <harryle.fit@gmail.com>
2017-09-15 16:10:09 +0700

    Update MainActivity.java

--- RxExample/app/src/main/java/com/example/harry/rxexample/MainActivity.java
+++ RxExample/app/src/main/java/com/example/harry/rxexample/MainActivity.java
@@ -43,46 +43,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-
-        RxTextView.afterTextChangeEvents(tvInput)
-                .filter(event -> !event.editable().toString().equals(""))
-                .map(event -> event.editable().toString())
-                .subscribe(inputText -> {
-                    if (inputText.contains("aaa")) {
-                        tvLog.setText("AAA");
-                    } else {
-                        tvLog.setText("Ops");
-                    }
-                }, throwable -> {
-                    tvLog.setText(throwable.getLocalizedMessage());
-                });
-
-//        tvInput.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                try {
-//                    if (!s.toString().equals("")) {
-//                        if (s.toString().contains("aaa")) {
-//                            tvLog.setText("AAA");
-//                        } else {
-//                            tvLog.setText("Ops");
-//                        }
-//                    }
-//                }catch (Exception ex){
-//                    tvLog.setText(ex.getLocalizedMessage());
-//                }
-//            }
-//        });
         cats = new ArrayList<>();
         cats.add(new Cat("Kathy", true, true, false));
         cats.add(new Cat("Kitty", false, false, false));
@@ -109,19 +69,4 @@ public class MainActivity extends AppCompatActivity {
         //TODO 5. neu text cua tvInput thay doi va co chua chuoi ki tu 'aaa' -> log ra 'Yeah', nguoc lai 'Ops' bang tvLog
 
         //TODO 6. dung tvInput log ra : 'Valid' khi ca tvInput vaf tvInput2 deu chua chuoi ki tu 'xxx', nguoc lai log ra: 'Invalid'
-        Observable.combineLatest(RxTextView.afterTextChangeEvents(tvInput), RxTextView.afterTextChangeEvents(tvInput2), new Func2<TextViewAfterTextChangeEvent, TextViewAfterTextChangeEvent, Boolean>() {
-            @Override
-            public Boolean call(TextViewAfterTextChangeEvent event, TextViewAfterTextChangeEvent event2) {
-                return event.editable().toString().contains("xxx") && event.editable().toString().contains("xxx");
-            }
-        }).subscribe(aBoolean -> {
-            if (aBoolean) {
-                tvLog.setText("Valid");
-            } else {
-                tvLog.setText("InValid");
-            }
-        }, throwable -> {
-            tvLog.setText(throwable.getLocalizedMessage());
-        });
-    }
 }
